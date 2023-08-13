8b30574845aead4b3dc3adadb0b342963ebc7c6d
akshaygothe <akshaygothe56@gmail.com>
2023-08-13 11:05:04 +0530

    injecting custom properties

--- src/main/java/com/akshay/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/akshay/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,11 +1,19 @@
 package com.akshay.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
+	// injecting properties
+	@Value("${coach.name}")
+	private String CoachName;
+	
+	@Value("${team.name}")
+	private String teamName;
+	
 	@GetMapping("/")
 	public String sayHello() {
 		return "Hello World";
@@ -14,5 +22,10 @@ public class FunRestController {
 	@GetMapping("/workout")
 	public String getDaliyWorkout() {
 		return "Run Hard 5K!";
+	}
+	
+	@GetMapping("/teaminfo")
+	public String getTeamInfo() {
+		return "Coach: " + CoachName + ", Team Name: " + teamName;
 	}
 }
